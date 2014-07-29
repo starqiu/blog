48bb720f9c24ad721c05f380b7e2b5cecf7bc8bd
starqiu <674867812@qq.com>
2014-07-29 16:54:49 +0800

    finish CRU of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,16 @@
 class ArticlesController < ApplicationController
 	def new
-
+	  @article = Article.new
 	end
-
+	 
 	def create
-		@article = Article.new(article_params)
-		@article.save
-
-		redirect_to @article
+	  @article = Article.new(article_params)
+	 
+	  if @article.save
+	    redirect_to @article
+	  else
+	    render 'new'
+	  end
 	end
 
 	def show
@@ -16,6 +19,20 @@ class ArticlesController < ApplicationController
 
 	def index
 		@articles = Article.all
+	end
+
+	def edit
+	  @article = Article.find(params[:id])
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
 	end
 
 	private 
